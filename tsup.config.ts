68d2409017580b05509d251881dd7a5cc9326494
naporin0624 <naporin0624@gmail.com>
2024-09-17 02:54:53 +0000

    chore: format

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: {
     index: 'src/broadcast-message.do/index.ts',
     'helpers/upgrade': 'src/middleware/upgrade/index.ts',
-    "helpers/client": "src/client/index.ts",
+    'helpers/client': 'src/client/index.ts',
   },
   external: ['hono', /cloudflare:/],
   format: ['cjs', 'esm'],
