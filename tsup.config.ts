9573ea4d38a37b117707e188c3f4d2b322302015
naporin0624 <naporin0624@gmail.com>
2024-10-31 02:24:05 +0000

    chore: add minify build pipeline

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,11 +6,31 @@ export default defineConfig({
     'helpers/upgrade': 'src/middleware/upgrade/index.ts',
     'helpers/client': 'src/client/index.ts',
   },
+  dts: {
+    banner: "/// <reference types='@cloudflare/workers-types' />",
+  },
   external: ['hono', /cloudflare:/],
   format: ['cjs', 'esm'],
   clean: true,
   sourcemap: true,
-  dts: {
-    banner: "/// <reference types='@cloudflare/workers-types' />",
+  minify: 'terser',
+  treeshake: true,
+  terserOptions: {
+    toplevel: true,
+    ecma: 2020,
+    compress: {
+      passes: 3,
+      drop_console: true,
+      drop_debugger: true,
+      pure_getters: true,
+      unsafe: true,
+      keep_fargs: true,
+      keep_fnames: true,
+      unused: true,
+      dead_code: true,
+    },
+    module: true,
+    keep_classnames: true,
+    keep_fnames: true,
   },
 });
